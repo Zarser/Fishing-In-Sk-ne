33e4cfb915eb9db7844822d0ffa029918c427dd7
Zarser <k.leandersson@live.se>
2022-01-18 11:36:44 +0100

    My first Version,

---
+++ javascript.js
@@ -0,0 +1,19 @@
+function calc()
+{  
+
+    var längd = (document.getElementsByClassName("Lä").value);
+    var vikt = (document.getElementsByClassName("Vi").value);
+    
+if (längd>0&vikt>0);
+				fultonform.getElementById("Submit").value=(vikt*100)/(längd*längd*längd);
+
+
+
+
+
+
+
+
+
+
+
