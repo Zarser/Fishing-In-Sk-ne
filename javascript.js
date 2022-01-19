0363985e9b6268e5ba39f694cbd651fca62ea035
Kristoffer Hansson <k.leandersson@live.se>
2022-01-19 10:54:00 +0100

    UPDATE
    *New calculator with slide fuction

--- javascript.js
+++ javascript.js
@@ -1,13 +1,67 @@
 
-    var Längd = document.getElementsById("Längd").value;
-    var Vikt = document.getElementsById("Vikt").value;
+var slider = document.getElementById("fulton");
+var output = document.getElementById("längd");
+var inputbox = document.getElementById("värdelängd");
+var slider1 = document.getElementById("fulton1");
+var output1 = document.getElementById("vikt");
+var inputbox1 = document.getElementById("värdevikt");
+
+
+
+output.addEventListener = slider.value;
+
+slider.oninput = function(){
+    inputbox.value = this.value;
+}
+
+output1.addEventListener = slider1.value;
+
+slider1.oninput = function(){
+    inputbox1.value = this.value;
+}
+
+
+function calc ()
+{
+    var längd = parseInt(slider.value);
+    var vikt = parseInt(slider1.value);
     
-    function calc ()
-    {
-        var Längd = parseInt(Längd.value);
-        var Vikt = parseInt(Vikt.value);
+
+    // var a = 1.34283423423423;
+    // output1.value = math.round(a);
 
 
-if (Längd>0&Vikt>0);
-				document.getElementById("Submit").value=(Vikt*100)/(Längd*Längd*Längd);
-    }
+    if (längd>0&vikt>0);
+				document.getElementById("värde").value=(vikt*100)/(längd*längd*längd);
+}
+
+   document.getElementById('fulton').onchange = function() {
+    // console.log("Hej1");
+    calc();
+  };
+  
+  document.getElementById('fulton1').onchange = function() {
+    //console.log("Hej11");
+    calc();
+  };
+  
+
+console.log(slider);
+
+
+slider.värde = function() {
+    inputbox.value=this.value;
+
+    calc();
+}
+
+
+
+slider1.värde = function() {
+    inputbox.value=this.value;
+    
+    calc();
+}
+
+
+
