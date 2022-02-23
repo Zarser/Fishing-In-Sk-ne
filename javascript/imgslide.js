d712ff20444370358c33f868883a307172cde0cc
Kristoffer Hansson <97549161+Zarser@users.noreply.github.com>
2022-02-23 19:46:48 +0100

    Changed menubar, extra shadow, more info imgslider

--- javascript/imgslide.js
+++ javascript/imgslide.js
@@ -16,19 +16,3 @@ function showDivs(n) {
   x[slideIndex-1].style.display = "block"; 
 } */
 
-var swiper = new Swiper(".mySwiper", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: "auto",
-  coverflowEffect: {
-    rotate: 50,
-    stretch: 0,
-    depth: 100,
-    modifier: 1,
-    slideShadows: true,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-});
